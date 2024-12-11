77061db964d0ac6d20b874ffeec6ac3dbf99431b
FiorenzoParascandolo1 <fiorenzo.parascandolo@gmail.com>
2024-12-11 11:20:33 +0100

    Update Scripts

---
+++ scripts/COGT_X-VLM+_inference.sh
@@ -0,0 +1,12 @@
+python main.py \
+--do_test \
+--xvlm \
+--lr 5e-4 \
+--validation_batch_size 16 \
+--num_workers 3 \
+--parser roberta \
+--resume \
+--n_layer 4 \
+--n_head 12 \
+--n_embd 768 \
+--dir_to_save_checkpoint path/to/load/checkpoint
